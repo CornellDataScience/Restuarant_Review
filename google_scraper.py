26b2706811c4255b0cd5e23601b3e108bb1cbb0b
Anoop Balakrishnan <banoop@gmail.com>
2019-02-24 15:56:11 -0500

    Google scraper reviewText found

--- google_scraper.py
+++ google_scraper.py
@@ -17,8 +17,14 @@ def scrapeGoogle(url):
         elem = driver.find_element_by_xpath(
             "//div[@class ='section-listbox section-scrollbox scrollable-y scrollable-show']")
         elem.send_keys(Keys.END)
-        time.sleep(2)
+        time.sleep(0.5)
 
-    soup = BeautifulSoup(driver.page_source)
-    print(soup.text)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    for reviewFull in soup.find_all('div', class_='section-review ripple-container'):
+        print(reviewFull.findChild('span',class_='section-review-text').text)
+        print('')
+
+
+
 scrapeGoogle('https://www.google.com/maps/place/Taverna+Banfi/@42.4464947,-76.4844149,17z/data=!3m1!4b1!4m10!1m2!2m1!1srestaurants+in+ithaca!3m6!1s0x89d0818b0848a40f:0xbabc00374d300924!8m2!3d42.4464908!4d-76.4822262!9m1!1b1')
