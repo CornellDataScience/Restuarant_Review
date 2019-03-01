383f469f620b73e962093de8db300c5036abef94
Anoop Balakrishnan <banoop@gmail.com>
2019-03-01 16:03:50 -0500

    Zomato Scraper updated and getRestaurants to newest first order

---
+++ Zomato_Web_Scraper.py
@@ -0,0 +1,65 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+import os
+from selenium import webdriver
+import time
+
+def scrapeZomato(url):
+    cwd = os.getcwd()
+    driverPath = cwd + '/chromedriver'
+    driver = webdriver.Chrome(executable_path=driverPath)
+    driver.get(url)
+    time.sleep(3)
+    elem = driver.find_element_by_xpath("//div[@class ='ui segment clearfix zs-load-more res-page-load-more']")
+    while(elem != None):
+
+        try:
+            elem.click()
+
+            elem = driver.find_element_by_xpath("//div[@class ='ui segment clearfix zs-load-more res-page-load-more']")
+        except:
+            print('Not found')
+            break
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    ReviewDict = {}
+    currentRestaurant = soup.find('a',class_='ui large header left').text
+    currentRestaurant = currentRestaurant.replace('\n','').strip()
+    for fullReview in soup.find_all('div', class_='ui segments res-review-body res-review clearfix js-activity-root mbti item-to-hide-parent stupendousact'):
+        ReviewID = int(fullReview['data-review_id'])
+        ReviewDict[ReviewID] = ['Yelp', currentRestaurant]
+        review = fullReview.findChild('div',class_='rev-text mbot0')
+        if(review != None):
+            reviewText = review.text
+            reviewText = reviewText.replace('POSITIVE', '')
+            reviewText = reviewText.replace('Rated', '')
+            reviewText = reviewText.strip()
+        dateDiv = fullReview.findChild('div',class_='fs12px pbot0 clearfix',recursive=True)
+        timeTag = dateDiv.findChild('time',recursive=True)
+        ReviewDict[ReviewID].append(timeTag['datetime'])
+        ReviewDict[ReviewID].append(reviewText)
+        ratingDiv = fullReview.findChild('div', class_='ttupper fs12px left bold zdhl2 tooltip icon-font-level-7')
+        if (ratingDiv != None):
+            ratingText = ratingDiv['aria-label']
+            ratingText = ratingText.replace('Rated', '')
+            ratingText = ratingText.strip()
+            rating = float(ratingText)
+            ReviewDict[ReviewID].append(rating)
+        else:
+            ReviewDict[ReviewID].append(None)
+        voteDiv = fullReview.findChild('div',class_='left mr5 ui tiny labeled button js-btn-thank',recursive=True)
+        voteCount = voteDiv['data-likes']
+        if(voteCount == ''):
+            voteCount = 0
+        else:
+            voteCount = int(voteCount)
+        ReviewDict[ReviewID].append(voteCount)
+        #fullReview.findChild('span')
+
+
+    return ReviewDict
+
+scrapeZomato('https://www.zomato.com/finger-lakes-region-ny/collegetown-bagels-3-ithaca')
+
+
