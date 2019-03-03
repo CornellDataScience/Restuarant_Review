cea52cdae0df547ec774add871efde92e85ba935
EageRAssassin <rt389@cornell.edu>
2019-03-03 15:00:41 -0500

    yelp scrapper with processes created

--- YelpBaselineScrapper.py
+++ YelpBaselineScrapper.py
@@ -1,5 +1,34 @@
-import GetRestaurantsYelpAPI
-# use yelp api to
-fullDict = getRestaurants('Ithaca')
-for elem in fullDict:
-    print(elem + ' - ' + fullDict[elem])
+import json
+from GetRestaurantsYelpAPI import getRestaurants
+from yelp_scraper import scrapeYelp
+from multiprocessing import Process
+
+def scrape_for_url(url):
+    print('start scrapping for url' + url)
+    scrape_data = scrapeYelp(url)
+    print(scrape_data)
+    with open('YelpData.txt', 'a+') as outfile:
+        json.dump(scrape_data, outfile)
+        outfile.write("\n")
+
+
+# use yelp api to scrape Yelp data simultaneously
+def main():
+    processes = []
+
+    name_url_dict = getRestaurants('Ithaca')
+
+    # create new YelpData.txt file
+    open('YelpData.txt', 'w').close()
+
+    # create new processes for all urls and start scrape at the same time
+    for elem in name_url_dict:
+        p = Process(target=scrape_for_url, args=(name_url_dict[elem],))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+main()
