0710a21e1076a9a8281d90fe9dfc02a257bd6a5c
Cornell Data Science <dk444@cornell.edu>
2019-05-03 12:55:33 -0400

    Changing logs

--- post_to_frontend.py
+++ post_to_frontend.py
@@ -2,12 +2,8 @@ import requests
 from dbms import initialize_yelp, initialize_zomato
 import json 
 
-yelp_df = initialize_yelp()
-zomato_df = initialize_zomato()
-print(yelp_df)
-yelp_json = yelp_df.to_json(orient='split')
-zomato_json = zomato_df.to_json(orient='split')
-
-data = json.dumps({"yelp":yelp_json, "zomato": zomato_json})
-r = requests.post("http://localhost:5000/data",data=data)
-print(r.text)
+def send_data(yelp_df, zomato_df):
+	yelp_json = yelp_df.to_json(orient='split')
+	zomato_json = zomato_df.to_json(orient='split')
+	data = json.dumps({"yelp":yelp_json, "zomato": zomato_json})
+	r = requests.post("http://128.84.48.178:5000/data",data=data)
