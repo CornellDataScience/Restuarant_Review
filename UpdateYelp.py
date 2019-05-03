3eaa68fcb23172a15fc4b282edcb4535af24abfb
Cornell Data Science <dk444@cornell.edu>
2019-05-03 12:09:31 -0400

    change updateYelp and updateZomato

--- UpdateYelp.py
+++ UpdateYelp.py
@@ -1,5 +1,13 @@
 from KafkaTopics import update_yelp
 import dbms
+from post_to_frontend import send_data
 
-df = dbms.initialize_yelp()
-update_yelp(df)
+yelp_df = dbms.initialize_yelp()
+update_yelp(yelp_df)
+
+zomato_df = dbms.initialize_zomato()
+send_data(yelp_df, zomato_df)
+
+
+
+
--- UpdateZomato.py
+++ UpdateZomato.py
@@ -1,5 +1,14 @@
 from KafkaTopics import update_zomato
 import dbms
+from post_to_frontend import send_data
 
-df = dbms.initialize_zomato()
-update_zomato(df)
+
+zomato_df = dbms.initialize_zomato()
+update_zomato(zomato_df)
+
+yelp_df = dbms.initialize_yelp()
+send_data(yelp_df, zomato_df)
+
+
+
+
